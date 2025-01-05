9d4540e3ea64fc9b72b8d81bced12a705e6a77fd
LigijaLiva <ligijaliva@gmail.com>
2025-01-05 18:26:25 +0200

    Update test_main.py
    Izlaboju kļūdas kodā

--- tests/test_main.py
+++ tests/test_main.py
@@ -27,20 +27,20 @@ def db():
     os.remove(TEST_DB)
 
 def test_add_contact(db):
-    add_contact('Anna Liepa', 'anna@example.com', '+37198765432')
+    add_contact('Anna Liepkalna', 'annali@example.com', '+37128765432')
     cursor = db.cursor()
-    cursor.execute('SELECT * FROM contacts WHERE email = ?', ('anna@example.com',))
+    cursor.execute('SELECT * FROM contacts WHERE email = ?', ('annali@example.com',))
     result = cursor.fetchone()
     assert result is not None
     assert result[1] == 'Anna Liepkalna'
-    assert result[2] == 'annaliep@example.com'
+    assert result[2] == 'annali@example.com'
     assert result[3] == '+37128765432'
 
 def test_view_contacts(db, capsys):
     view_contacts()
     captured = capsys.readouterr()
     assert 'Anna Liepkalna' in captured.out
-    assert 'annaliep@example.com' in captured.out
+    assert 'annali@example.com' in captured.out
     assert '+37128765432' in captured.out
 
 def test_update_contact(db):
